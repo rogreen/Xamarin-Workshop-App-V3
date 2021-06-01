f57a46c7686e58d6092083c77b7ea3d8387224bf
Robert <rgreen2005@msn.com>
2021-06-01 14:53:35 -0600

    Add project files.

---
+++ HoneyDo/HoneyDo/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using HoneyDo.Services;
+using HoneyDo.ViewModels;
+using Xamarin.Forms;
+
+namespace HoneyDo
+{
+    public partial class App : Application
+    {
+        public static HoneyDoItemViewModel SelectedItemViewModel;
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<SQLiteDataStore>();
+            //DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
